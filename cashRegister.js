095d03d07074eeb5da3dba9ea90faaed0adfb8b4
deepeshgotiya <deepeshgotiya79@gmail.com>
2021-12-07 15:33:38 +0530

    fixed-for-negative-values

--- cashRegister.js
+++ cashRegister.js
@@ -11,18 +11,31 @@ const availableNotes = [2000,500,100,20,10,5,1]
 function validateAmount(){
     let billAmountValue = Number(billAmount.value)
     let givenCashValue =Number(givenCash.value)
-    if(billAmountValue <= givenCashValue){
-        const returnAmount = givenCashValue - billAmountValue;
-        if(returnAmount == 0){
-            errorBox.innerText = "No Amount to return"
-        }else{
-            errorBox.innerText = returnAmount;
-            calculateChange(returnAmount)
+    if(billAmountValue > 0 && givenCashValue > 0){
+        if(billAmountValue <= givenCashValue){
+            const returnAmount = givenCashValue - billAmountValue;
+            if(returnAmount == 0){
+                errorBox.innerText = "No Amount to return"
+            }else{
+                errorBox.innerText = returnAmount;
+                calculateChange(returnAmount)
+            }
         }
-    }else{
+    }else if(billAmountValue < 0 && givenCashValue < 0){
         errorBox.innerText = "Given Amount should be greater or equal to Bill Amount"
-    }
-    }
+    }}
+    // if(billAmountValue <= givenCashValue){
+    //     const returnAmount = givenCashValue - billAmountValue;
+    //     if(returnAmount == 0){
+    //         errorBox.innerText = "No Amount to return"
+    //     }else{
+    //         errorBox.innerText = returnAmount;
+    //         calculateChange(returnAmount)
+    //     }
+    // }else{
+    //     errorBox.innerText = "Given Amount should be greater or equal to Bill Amount"
+    // }
+    // }
 function calculateChange (returnAmount){
         for (let i = 0; i < availableNotes.length; i++) {
           const numberOfNotes = Math.trunc(returnAmount / availableNotes[i]);
